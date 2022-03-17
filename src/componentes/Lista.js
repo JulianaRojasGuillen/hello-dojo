8d8c93e576d29d5e257608615b480f91f55b8609
JulianaRojasGuillen <juliana.rojas@pucp.pe>
2022-03-17 08:49:59 -0500

    Primer React con JSX terminado

---
+++ src/componentes/Lista.js
@@ -0,0 +1,14 @@
+const Lista =()=>{
+    return (
+        <div>
+            <ul>
+                <li>Learn React</li>
+                <li>Climb Mt. Everest</li>
+                <li>Run a marathon</li>
+                <li>Feed the dogs</li>
+            </ul>
+        </div>
+    );
+}
+
+export default Lista;
